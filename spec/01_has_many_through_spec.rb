f1bde50e92bd948ac367dbd16ae77111b9ba57d2
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-31 23:46:55 +0000

    Automatically backed up by Learn

--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -257,7 +257,24 @@ describe "Object relationships" do
     end
 
     describe "#avg_tips" do
-      it ""
+      it "returns the avg of tips a waiter makes" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+        terrance = Waiter.new("Terrance", 1)
+
+
+        meal_1 = howard.new_meal(terrance, 15, 2)
+        meal_2 = daniel.new_meal(terrance, 15, 4)
+        meal_3 = lisa.new_meal(terrance, 15, 5)
+        meal_4 = josh.new_meal(terrance, 15, 1)
+        meal_5 = steven.new_meal(terrance, 15, 3)
+        meal_6 = lisa.new_meal(terrance, 15, 3)
+
+        expect
+      end
     end
   end
 end
