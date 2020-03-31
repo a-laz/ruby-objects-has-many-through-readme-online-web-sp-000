b80f8993ca2760307a486ef0c8d6fcaf91627999
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-31 23:06:33 +0000

    Automatically backed up by Learn

--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -194,5 +194,20 @@ describe "Object relationships" do
         expect(terrance.best_tipper).to eq(lisa)
       end
     end
+
+    describe "#customers" do 
+      it "returns an array of all the waiter's customers"
+      howard = Customer.new("Howard", 30)
+      daniel = Customer.new("Daniel", 30)
+      terrance = Waiter.new("Terrance", 1)
+      joe = Waiter.new("Joe", 10)
+      esmery = Waiter.new("Esmery", 2)
+      andrew = Waiter.new("Andrew", 3)
+      
+      howard.new_meal(terrance, 15, 2)
+      howard.new_meal(joe, 15, 4)
+      howard.new_meal(andrew, 15, 5)
+      daniel.new_meal(terrance, 20, 1)
+      daniel.new_meal(esmery, 15, 3)
   end
 end
